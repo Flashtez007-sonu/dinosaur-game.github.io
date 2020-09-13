586899e45a2e53e192cdb34bae594a4a67126588
Shubham Gupta <68388052+Flashtez007-sonu@users.noreply.github.com>
2020-09-13 11:46:42 +0530

    javascript file

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump()
+{
+    if(character.classList.contains ("animate")){
+        return
+        // character.classList.add("animate");
+    }
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
+ var checkdead = setInterval(function()
+ {
+     var characterTop = parseInt(window.getComputedStyle(character)
+     .getPropertyValue("top"));
+
+     var blockleft = parseInt(window.getComputedStyle(block)
+     .getPropertyValue("left"));
+
+     if(blockleft<20 && blockleft>0 && characterTop>=120){
+         block.style.animation = "none";
+         block.style.display = "none";
+         alert(" u lose.");
+     }
+     
+ }, 10);
